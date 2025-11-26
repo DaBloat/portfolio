9ca6af2311f52d9ec5047aeb591d322491c4652f
DaBloat <kurtrusselvillamor1206@gmail.com>
2025-11-26 20:23:52 +0800

    changes to the home page!

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,7 +1,22 @@
+import pose from '../assets/pose.png'
+import '../style/home.css'
+
 export const Home = () => {
     return(
         <section id='me' className="section home-sec">
-            <h1>Home</h1>
+            <div className='home-sec-holder'>
+                <div>
+                    <h2 className='greeting'>kurt@portfolio:~$ whoami</h2>
+                    <h1 className='name'>Kurt Russel</h1>
+                    <h2 className='greeting'>kurt@portfolio:~$ current-status</h2>
+                    <h1 className='name'>Computer Engineering Student</h1>
+                    <h2 className='greeting'>kurt@portfolio:~$ cat interests.txt</h2>
+                    <h1 className='name'>Data Science | Linux Enthusiast | Dev</h1>
+                </div>
+                <div>
+                    <img className='pose' src={pose}></img>
+                </div>
+            </div>
         </section>
     )
 }
